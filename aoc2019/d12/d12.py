6233d2744bd4dfd853c7a7fb0311a9698e1a69d4
gremus <gabriel.grld@gmail.com>
2021-08-27 15:32:34 -0300

    d12 works on tests but takes forever on real input

--- aoc2019/d12/d12.py
+++ aoc2019/d12/d12.py
@@ -7,6 +7,7 @@ Created on 02-mai-2020
 
 from itertools import permutations
 
+
 class Vector(tuple):
     '''
     Creates a subclass to treat tuples as if they were vectors.
@@ -16,23 +17,19 @@ class Vector(tuple):
     def __new__(cls, x, y, z=0):
         return super().__new__(cls, (x, y, z))
 
-    
     def __init__(self, x, y, z):
         self.x = self[0]
         self.y = self[1]
         self.z = self[2]
 
-    
     def __len__(self):
         return sum(abs(_) for _ in self)
-
 
     def __add__(self, value):
         x = self[0] + value[0]
         y = self[1] + value[1]
         z = self[2] + value[2]
         return Vector(x, y, z)
-
 
     def __sub__(self, value):
         x = self[0] - value[0]
@@ -46,12 +43,9 @@ class Moon:
         init_pos
         self.r = Vector(*init_pos)
         self.v = Vector(0, 0, 0)
-        self.energy = len(self.r) * len(self.v)
-
 
     def __repr__(self):
         return f"<pos={self.r}, vel={self.v}>"
-
 
     def update_velocity(self, another_moon):
         for i in range(3):
@@ -63,17 +57,24 @@ class Moon:
             elif self.r[i] > another_moon.r[i]:
                 self.v -= dv
 
-
     def update_position(self):
         self.r += self.v
-        self.energy = len(self.r) * len(self.v)
+
+    def calc_energy(self):
+        return len(self.r) * len(self.v)
 
 
 class MoonsSystem(list):
     def __init__(self, positions_file):
         self._read_input(positions_file)
-        self.set_initial_state()
+        self.t = 0
+        super().__init__([Moon(pos) for pos in self.init_pos])
 
+    def __repr__(self):
+        out = ""
+        for moon in self:
+            out += repr(moon) + "\n"
+        return out
 
     def _read_input(self, fname):
         def input_to_tuple(pos):
@@ -88,48 +89,45 @@ class MoonsSystem(list):
             positions = f.readlines()
         self.init_pos = [input_to_tuple(pos) for pos in positions]
 
-
-    def set_initial_state(self):
-        super().__init__([Moon(pos) for pos in self.init_pos])
-        self.system_energy = sum(m.energy for m in self)
-
-
     def time_step(self):
         for i,j in permutations(range(len(self)), 2):
             self[i].update_velocity(self[j])
-    
-        self.system_energy = 0
         for moon in self:
             moon.update_position()
-            self.system_energy += moon.energy
+        self.t += 1
+
+    def calc_energy(self):
+        energy = 0
+        for moon in self:
+            energy += moon.calc_energy()
+        return energy
+
+
+def gcd(x, y):
+    return y if x==0 else gcd(y%x, x)
+
+
+def lcm(x, y):
+    return x*y // gcd(x, y)
 
 
 def get_periods(moons):
-    def bool_prod(bool_dict):
-        result = True
-        for key in bool_dict:
-            for element in bool_dict[key]:
-                result = (result and element)
-        return result
-    
-    axes = dict()
-    for i in range(3):
-        axes[i] = [[(moon.r, moon.v)] for moon in moons]
-
-    done = {0: [False, False, False, False],
-            1: [False, False, False, False], 
-            2: [False, False, False, False]} 
-
-    while not bool_prod(done):
-        moons.time_step()
+    p = [False, False, False]
+    ax = {0: [], 1: [], 2: []}
+            
+    while True:
         for i in range(3):
-            for j,moon in enumerate(moons):
-                if not done[i][j]:
-                    axes[i][j].append((moon.r[i], moon.v[i]))
-                    if (axes[i][j][-1] in axes[i][j][:-1]):
-                        done[i][j] = True
-
-    return axes, done
+            s = (tuple(moon.r[i] for moon in moons),
+                 tuple(moon.v[i] for moon in moons))
+            if (s in ax[i]) and (not p[i]):
+                p[i] = len(set(ax[i]))
+                print(p)
+            ax[i].append(s)
+        if all(p):
+            break
+        moons.time_step()
+    period = lcm(lcm(p[0], p[1]), p[2])
+    return period
 
 
 def part_one():
@@ -139,8 +137,11 @@ def part_one():
     while step < 1000:
         moons.time_step()
         step += 1
-    return moons.system_energy
+    return moons.calc_energy()
 
 
 def part_two():
     moons = MoonsSystem('input.dat')
+    # sit and relax
+    p = get_periods(moons)
+    return p
